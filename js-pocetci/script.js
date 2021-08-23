16738cb49b63c830a5522a06023180e7ea2cd09f
Mateo Vuković <vukovicmateo7ce@gmail.com>
2021-08-23 20:32:07 +0200

    JS 1

---
+++ js-pocetci/script.js
@@ -0,0 +1,61 @@
+/***************
+VALUES AND VARIABLES
+****************/
+/*
+let firstName = "mateo";
+let favouriteNumber = 7;
+
+console.log(firstName, favouriteNumber);
+
+// Variable naming convention and rules
+
+let firstNameAndLastName = "Mateo Vuković";
+// let 3years = 6; Number can not be the first sign
+
+
+
+let y = "teacher"; Not like this
+let myFirstJob = "programmer"; Instead like this
+
+
+
+firstName = "Darko";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+*/
+
+/***************
+DATA TYPES
+****************/
+
+/*
+STRING -> text 
+NUMBER -> whole and decimals 3.0
+BOOLEAN -> true or false
+UNDEFINED -> value of a variable whoe doesn't yet have a value
+NULL -> simmilar to undefined for now...
+
+
+let firstName = "Dario";
+let number1 = 14;
+let hasDriverLicence = false;
+let lastName;
+
+console.log(typeof firstName);
+console.log(typeof number1);
+console.log(typeof hasDriverLicence);
+console.log(typeof lastName);
+*/
+
+/***************
+let,const,var
+****************/
+
+//var firstName = "Mateo"; Not like this
+let firstName = "Dario";
+const PI = 3.1472;
+console.log(firstName);
+console.log(lastName);
